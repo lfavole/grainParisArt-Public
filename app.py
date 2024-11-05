3fbc210fce7e1585537f9fc0e8becb0fadf745c9
Laurent FAVOLE <laurentfavole03@gmail.com>
2024-11-05 18:51:22 +0100

    Code simplification and refactoring

--- app.py
+++ app.py
@@ -1,132 +1,106 @@
-from flask import Flask, render_template, request
+from dataclasses import dataclass
 from datetime import datetime, timedelta
 
-# IMPORT DES MODULES 
-from modules.Classes import *
+from flask import Flask, abort, render_template, request
 
-cinemas = {
-    "C0071": "Écoles Cinéma Club",
-    "C2954": "MK2 Bibliothèque",
-    "C0050": "MK2 Beaubourg",
-    "W7504": "Épée de bois",
-    "C0076": "Cinéma du Panthéon",
-    "C0089": "Max Linder Panorama",
-    "C0013": "Luminor Hotel de Ville",
-    "C0072": "Le Grand Action",
-    "C0099": "MK2 Parnasse",
-    "C0073": "Le Champo",
-    "C0020": "Filmothèque du Quartier Latin",
-    "C0074": "Reflet Medicis",
-    "C0159": "UGC Ciné Cité Les Halles",
-    "C0026": "UGC Ciné Cité Bercy"
-}
+# IMPORT DES MODULES
+from modules.Classes import Movie, Showtime, Theater
 
-theaters: list[Theater] = []
-for id, name in cinemas.items():
-    theaters.append(Theater({
-        "name": name,
-        "internalId": id,
-        "location": None
-    }))
+theaters = [
+    Theater("C0071", "Écoles Cinéma Club", None, 2.348973, 48.848363),
+    Theater("C2954", "MK2 Bibliothèque", None, 2.375488, 48.832448),
+    Theater("C0050", "MK2 Beaubourg", None, 2.352312, 48.861584),
+    Theater("W7504", "Épée de bois", None, 2.349555, 48.841300),
+    Theater("C0076", "Cinéma du Panthéon", None, 2.342385, 48.847488),
+    Theater("C0089", "Max Linder Panorama", None, 2.344856, 48.871370),
+    Theater("C0013", "Luminor Hotel de Ville", None, 2.353602, 48.858676),
+    Theater("C0072", "Le Grand Action", None, 2.352129, 48.847530),
+    Theater("C0099", "MK2 Parnasse", None, 2.330526, 48.842813),
+    Theater("C0073", "Le Champo", None, 2.343223, 48.849980),
+    Theater("C0020", "Filmothèque du Quartier Latin", None, 2.342790, 48.849510),
+    Theater("C0074", "Reflet Medicis", None, 2.342790, 48.849510),
+    Theater("C0159", "UGC Ciné Cité Les Halles", None, 2.343014, 48.849777),
+    Theater("C0026", "UGC Ciné Cité Bercy", None, 2.546596, 48.840113),
+]
+
 
 def getShowtimes(date):
-    showtimes:list[Showtime] = []
+    showtimes: list[Showtime] = []
 
     for theater in theaters:
         showtimes.extend(theater.getShowtimes(date))
 
-    data = {}
+    movies: list[Movie] = []
 
     for showtime in showtimes:
         movie = showtime.movie
         theater = showtime.theater
 
-        if showtime.movie.title not in data.keys():
-            data[movie.title] = {
-                "title": movie.title,
-                "duree": movie.runtime,
-                "genres": ", ".join(movie.genres),
-                "casting": ", ".join(movie.cast),
-                "realisateur": movie.director,
-                "synopsis": movie.synopsis,
-                "affiche": movie.affiche,
-                "director": movie.director,
-                "wantToSee": movie.wantToSee,
-                "url": f"https://www.allocine.fr/film/fichefilm_gen_cfilm={movie.id}.html",
-                "seances": {}
-            }
+        if movie not in movies:
+            movies.append(movie)
 
-            
-        if theater.name not in data[movie.title]["seances"].keys():
-            data[movie.title]["seances"][theater.name] = []
+        movie.seances[theater.name].append(showtime.startsAt.strftime("%H:%M"))
 
-        data[movie.title]["seances"][theater.name].append(showtime.startsAt.strftime("%H:%M"))
+    movies = sorted(movies, key=lambda x: x.wantToSee, reverse=True)
 
-    data = data.values()
+    return movies
 
-    data = sorted(data, key=lambda x: x["wantToSee"], reverse=True)
-
-    return data
 
 showtimes = []
-for i in range(0, 7):
-    day_showtimes = getShowtimes(datetime.today()+timedelta(days=i))
+for i in range(7):
+    day_showtimes = getShowtimes(datetime.today() + timedelta(days=i))
     showtimes.append(day_showtimes)
     print(f"{len(day_showtimes)} séances récupéré {i+1}/7!")
 
 app = Flask(__name__)
 
+
 def translateMonth(num: int):
-    match num:
-        case 1: return "janv"
-        case 2: return "févr"
-        case 3: return "mars"
-        case 4: return "avr"
-        case 5: return "mai"
-        case 6: return "juin"
-        case 7: return "juil"
-        case 8: return "août"
-        case 9: return "sept"
-        case 10: return "oct"
-        case 11: return "nov"
-        case 12: return "déc"
-        case _: return "???"
+    return ["janv", "févr", "mars", "avr", "mai", "juin", "juil", "août", "sept", "oct", "nov", "déc"][num - 1]
+
 
 def translateDay(weekday: int):
-    match weekday:
-        case 0: return "lun"
-        case 1: return "mar"
-        case 2: return "mer"
-        case 3: return "jeu"
-        case 4: return "ven"
-        case 5: return "sam"
-        case 6: return "dim"
-        case _: return "???"
+    return ["lun", "mar", "mer", "jeu", "ven", "sam", "dim"][weekday]
 
-@app.route('/health')
+
+@app.route("/health")
 def health():
     return "OK"
 
-@app.route('/')
+
+@dataclass
+class Day:
+    index: int
+    _date: datetime
+    choisi: bool = False
+
+    @property
+    def jour(self):
+        return translateDay(self._date.weekday())
+
+    @property
+    def chiffre(self):
+        return self._date.day
+
+    @property
+    def mois(self):
+        return translateMonth(self._date.month)
+
+
+@app.route("/")
 def home():
     delta = request.args.get("delta", default=0, type=int)
 
-    if delta > 6: delta = 6
-    if delta < 0: delta = 0
+    if not (0 <= delta <= 6):
+        abort(404)
 
     dates = []
 
-    for i in range(0,7):
-        day = datetime.today()+timedelta(i)
-        dates.append({
-            "jour": translateDay(day.weekday()),
-            "chiffre": day.day,
-            "mois": translateMonth(day.month),
-            "choisi": i==delta,
-            "index": i
-        })
+    for i in range(7):
+        dates.append(Day(i, datetime.today() + timedelta(i), i == delta))
 
-    return render_template('index.html', page_actuelle='home', films=showtimes[delta], dates=dates)
+    return render_template("index.html", page_actuelle="home", films=showtimes[delta], dates=dates, theaters=theaters)
 
-if __name__ == '__main__':
-    app.run() 
+
+if __name__ == "__main__":
+    app.run(debug=True)
